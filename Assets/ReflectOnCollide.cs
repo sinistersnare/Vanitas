867cd87d6e0d57d3d976412c1110592b96fa5518
FrozenNoodles <tarn.audrey@gmail.com>
2019-03-29 23:04:11 -0400

    Pretty cropping

---
+++ Assets/ReflectOnCollide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectOnCollide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // check if obj is player object
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 100000), ForceMode2D.Force);
+        }
+    }
+}
