757af1fd297b05ac7bf28b2571e6c447e0205d31
Davis Silverman <davis.silverman@radiantsolutions.com>
2019-03-29 20:28:09 -0400

    v0 for real this time

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 10;
+
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        this.body = this.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+
+    }
+    void LateUpdate()
+    {
+        this.HandleMovement();
+
+    }
+
+    void MoveLocally(Vector3 moveDelta)
+    {
+        moveDelta = transform.TransformDirection(moveDelta);
+        body.MovePosition(this.transform.position + moveDelta);
+    }
+
+    void HandleMovement()
+    {
+        float horizontalMovement = 0, verticalMovement = 0;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            verticalMovement -= speed;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            verticalMovement += speed;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            horizontalMovement -= speed;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            horizontalMovement += speed;
+        }
+
+        Vector2 moveDirection = new Vector3(horizontalMovement, verticalMovement, 0);
+        this.MoveLocally(moveDirection);
+    }
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public CharacterController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.player.enabled = !this.player.enabled;
+        }
+    }
+}
